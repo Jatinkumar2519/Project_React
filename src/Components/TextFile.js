f4d226a1d8f17bea160946b365baac4bdb05709e
Jatinkumar2519 <149255592+Jatinkumar2519@users.noreply.github.com>
2025-07-31 19:11:33 +0530

    Text file component

---
+++ src/Components/TextFile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextFile(){
+    return(
+        <div>
+            <div class="mb-3">
+            <label for="TextFile" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="TextFile" rows="3"></textarea>
+            </div>
+        </div>
+    )
+}
