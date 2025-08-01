050c358974032030808ff9eb9fd05da401504f49
Jatinkumar2519 <149255592+Jatinkumar2519@users.noreply.github.com>
2025-08-01 19:37:26 +0530

    add summery

--- src/Components/TextFile.js
+++ src/Components/TextFile.js
@@ -1,37 +1,50 @@
-import React,{use, useState} from 'react'
+import React, { use, useState } from 'react'
 
-export default function TextFile(props){
-    const [text,setText] = useState("Enter text Here");
-    const handleUpCase = () =>{
+export default function TextFile(props) {
+    const [text, setText] = useState("Enter text Here");
+    const handleUpCase = () => {
         setText(text.toUpperCase());
     }
-    const handleDownCase = () =>{
+    const handleDownCase = () => {
         setText(text.toLowerCase());
     }
-    const handleCamelCase = () =>{
+    const handleCamelCase = () => {
         let str = "";
-        for(let i = 0;i < text.length;i++){
-            if(i === 0 || text[i - 1] === ' '){
+        for (let i = 0; i < text.length; i++) {
+            if (i === 0 || text[i - 1] === ' ') {
                 str += text[i].toUpperCase();
             }
-            else{
+            else {
                 str += text[i];
             }
         }
         setText(str);
     }
-    const handleOnChange = (event) =>{
+    const getCountWords = () => {
+        return text.split(" ").length - 1;
+    }
+    const getLengthOfText = () => {
+        return text.length;
+    }
+    const handleOnChange = (event) => {
         setText(event.target.value);
     }
-    return(
-        <div>
-            <div className="mb-3">
-                <h1>{props.heading}</h1>
-                <textarea className="form-control" value={text} onChange={handleOnChange}  id="TextFile" rows="5"></textarea>
-                <button style={{marginTop : "10px"}}  type="button" onClick={handleUpCase}  className="btn btn-primary">Convert to upper case</button>
-                <button style={{marginTop : "10px",marginLeft : "7px"}}  type="button" onClick={handleDownCase}  className="btn btn-primary">Convert to lower case</button>
-                <button style={{marginTop : "10px",marginLeft : "7px"}}  type="button" onClick={handleCamelCase}  className="btn btn-primary">Convert to Camel case</button>
+    return (
+        <>
+            <div className='container'>
+                <div className="mb-3">
+                    <h1 style={{ fontFamily: "cursive" }}>{props.heading}</h1>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="TextFile" rows="5"></textarea>
+                    <button style={{ marginTop: "10px", fontFamily: "cursive" }} type="button" onClick={handleUpCase} className="btn btn-primary">Convert to upper case</button>
+                    <button style={{ marginTop: "10px", marginLeft: "7px", fontFamily: "cursive" }} type="button" onClick={handleDownCase} className="btn btn-primary">Convert to lower case</button>
+                    <button style={{ marginTop: "10px", marginLeft: "7px", fontFamily: "cursive" }} type="button" onClick={handleCamelCase} className="btn btn-primary">Convert to Camel case</button>
+                </div>
             </div>
-        </div>
+            <div>
+                <h1 style={{ fontFamily: "cursive" }}>Text Summery</h1>
+                <h5 style={{ fontFamily: "cursive" }}>Number of Words : {getCountWords()}</h5>
+                <h5 style={{ fontFamily: "cursive" }}>Length of words : {getLengthOfText()}</h5>
+            </div>
+        </>
     )
 }
