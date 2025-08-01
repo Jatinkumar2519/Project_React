b3823ec0ce6506f7919a2f3ece40e6cfe1fc68e8
Jatinkumar2519 <149255592+Jatinkumar2519@users.noreply.github.com>
2025-08-01 19:12:39 +0530

    Bittons handling

--- src/Components/TextFile.js
+++ src/Components/TextFile.js
@@ -5,15 +5,32 @@ export default function TextFile(props){
     const handleUpCase = () =>{
         setText(text.toUpperCase());
     }
+    const handleDownCase = () =>{
+        setText(text.toLowerCase());
+    }
+    const handleCamelCase = () =>{
+        let str = "";
+        for(let i = 0;i < text.length;i++){
+            if(i === 0 || text[i - 1] === ' '){
+                str += text[i].toUpperCase();
+            }
+            else{
+                str += text[i];
+            }
+        }
+        setText(str);
+    }
     const handleOnChange = (event) =>{
         setText(event.target.value);
     }
     return(
         <div>
             <div className="mb-3">
-            <h1>{props.heading}</h1>
-            <textarea className="form-control" value={text} onChange={handleOnChange}  id="TextFile" rows="5"></textarea>
-            <button style={{marginTop : "10px"}}  type="button" onClick={handleUpCase}  className="btn btn-primary">Convert to upper case</button>
+                <h1>{props.heading}</h1>
+                <textarea className="form-control" value={text} onChange={handleOnChange}  id="TextFile" rows="5"></textarea>
+                <button style={{marginTop : "10px"}}  type="button" onClick={handleUpCase}  className="btn btn-primary">Convert to upper case</button>
+                <button style={{marginTop : "10px",marginLeft : "7px"}}  type="button" onClick={handleDownCase}  className="btn btn-primary">Convert to lower case</button>
+                <button style={{marginTop : "10px",marginLeft : "7px"}}  type="button" onClick={handleCamelCase}  className="btn btn-primary">Convert to Camel case</button>
             </div>
         </div>
     )
